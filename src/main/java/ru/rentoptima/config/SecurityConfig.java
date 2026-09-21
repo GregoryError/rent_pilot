978c7c8ae35c1c62da24309982329b0113c0bc1b
GregoryError <kolesnikoff2085@yandex.ru>
2026-09-21 11:45:20 +0300

    SecurityConfig import fix

--- src/main/java/ru/rentoptima/config/SecurityConfig.java
+++ src/main/java/ru/rentoptima/config/SecurityConfig.java
@@ -4,12 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import ru.rentoptima.service.AppUserDetailsService;
+import ru.rentoptima.security.AppUserDetailsService;
 
 @Configuration
 @RequiredArgsConstructor
